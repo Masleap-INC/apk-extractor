7d056dda4c47b2702c00e4edc286e09ec836bb7e
imammasleap <imam@masleap.io>
2022-04-05 14:30:08 +0600

    feat: appCard UI Updatede

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -44,7 +44,7 @@ class HomePage extends StatelessWidget {
             actions: [
               IconButton(
                   onPressed: () {
-                    Get.to(()=>SearchPage());
+                    Get.to(() => SearchPage());
                   },
                   icon: const Icon(
                     FontAwesomeIcons.search,
@@ -64,83 +64,172 @@ class HomePage extends StatelessWidget {
           body: Obx(() => controller.isLoading.value
               ? const Center(child: CircularProgressIndicator())
               : CupertinoScrollbar(
-                  child: ListView.separated(
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const Divider();
-                      },
+                  child: ListView.builder(
+                      // separatorBuilder: (BuildContext context, int index) {
+                      //   return const Divider();
+                      // },
                       itemBuilder: (BuildContext context, int position) {
                         Application app = controller.applicationList[position];
 
-                        return Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 7.5),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              (app is ApplicationWithIcon)
-                                  ? ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Image.memory(
-                                        app.icon,
-                                        height: 60,
-                                        width: 60,
-                                      ),
+                        return Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () {},
+                            borderRadius: BorderRadius.circular(5.0),
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 8),
+                              //height: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(5.0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.1),
+                                      spreadRadius: 3,
+                                      blurRadius: 5,
+                                      offset: const Offset(0, 2),
                                     )
-                                  : ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Image.asset(
-                                        appLogo,
-                                        height: 60,
-                                        width: 60,
-                                      ),
-                                    ),
-                              const SizedBox(
-                                width: 15,
+                                  ]
                               ),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width - 90,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      app.appName,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style:
-                                          Theme.of(context).textTheme.headline5,
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Container(
+                                        margin: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                                        padding: const EdgeInsets.all(5),
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey.withOpacity(0.1),
+                                          borderRadius:
+                                          BorderRadius.circular(5.0),
+                                        ),
+                                        child: (app is ApplicationWithIcon)
+                                            ? Image.memory(
+                                              app.icon,
+                                              height: 60,
+                                              width: 60,
+                                            ) : Image.asset(
+                                              appLogo,
+                                              height: 60,
+                                              width: 60,
+                                            ),
+                                      ),
+                                      Expanded(
+                                          flex: 4,
+                                          child: SizedBox(
+                                            height: 70,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                              crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Expanded(child: Text(
+                                                      app.appName,
+                                                        maxLines: 1,
+                                                        overflow: TextOverflow.ellipsis,
+                                                      style: Theme.of(context).textTheme.headline4
+                                                    ),),
+                                                    Padding(
+                                                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                                                      child: Material(
+                                                        color: Colors.transparent,
+                                                        child: InkWell(
+                                                          onTap: (){
+
+                                                          },
+                                                          borderRadius: BorderRadius.circular(5.0),
+                                                          child: Container(
+                                                            padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+                                                            decoration: BoxDecoration(
+                                                              color: scaffoldBackgroundLight,
+                                                              borderRadius: BorderRadius.circular(5.0),
+                                                            ),
+                                                            child: const Icon(
+                                                              FontAwesomeIcons.ellipsisH
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    )
+
+
+                                                  ],
+                                                ),
+                                                Container(
+                                                  width: double.infinity,
+                                                  padding:
+                                                  const EdgeInsets.all(5),
+                                                  margin:
+                                                  const EdgeInsets.fromLTRB(0, 5, 5, 0),
+                                                  decoration: BoxDecoration(
+                                                    color: Colors.blueGrey,
+                                                    borderRadius:
+                                                    BorderRadius.circular(
+                                                        5.0),
+                                                  ),
+                                                  child: Text(
+                                                    app.packageName,
+                                                    maxLines: 2,
+                                                    overflow: TextOverflow.ellipsis,
+                                                    style: const TextStyle(
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          )),
+                                    ],
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(5),
+                                    margin: const EdgeInsets.all(5),
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey
+                                          .withOpacity(0.1),
+                                      borderRadius:
+                                      BorderRadius.circular(5.0),
                                     ),
-                                    const Divider(),
-                                    Text(
-                                      app.packageName,
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style:
-                                          Theme.of(context).textTheme.subtitle1,
+                                    child: Text(
+                                        'Version: (${app.versionCode}) ${app.versionName}\n'
+                                            'Installed: ${DateTime.fromMillisecondsSinceEpoch(app.installTimeMillis).toString()}\n'
+                                            'Installed: ${DateTime.fromMillisecondsSinceEpoch(app.updateTimeMillis).toString()}',
                                     ),
-                                    const Divider(),
-                                    Text(
-                                      'Version: (${app.versionCode}) ${app.versionName}',
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style:
-                                          Theme.of(context).textTheme.bodyText1,
-                                    ),
-                                    const Divider(),
-                                    Text(
-                                      'Installed: ${DateTime.fromMillisecondsSinceEpoch(app.installTimeMillis).toString()}',
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    ),
-                                    const Divider(),
-                                    Text(
-                                      'Installed: ${DateTime.fromMillisecondsSinceEpoch(app.updateTimeMillis).toString()}',
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
+                                  ),
+
+                                  Row(
+                                    children: [
+                                      actionButton(
+                                        title: 'Open',
+                                        onPressed: () {
+                                          app.openApp();
+                                        }
+                                      ),
+                                      actionButton(
+                                          title: 'Setting',
+                                          onPressed: () {
+                                            app.openSettingsScreen();
+                                          }
+                                      ),
+                                      actionButton(
+                                          title: 'Extract',
+                                          onPressed: () {
+
+                                          }
+                                      ),
+                                    ],
+                                  )
+
+                                ],
+                              ),
+                            ),
                           ),
                         );
                       },
@@ -152,45 +241,31 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  void onAppClicked(BuildContext context, Application app) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(app.appName),
-            actions: <Widget>[
-              _AppButtonAction(
-                label: 'Open app',
-                onPressed: () => app.openApp(),
-              ),
-              _AppButtonAction(
-                label: 'Open app settings',
-                onPressed: () => app.openSettingsScreen(),
-              ),
-              _AppButtonAction(
-                label: 'Uninstall app',
-                onPressed: () async => app.uninstallApp(),
-              ),
-            ],
-          );
-        });
-  }
-}
-
-class _AppButtonAction extends StatelessWidget {
-  final String label;
-  final VoidCallback? onPressed;
-
-  _AppButtonAction({required this.label, this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        onPressed?.call();
-        Navigator.of(context).maybePop();
-      },
-      child: Text(label),
+  Widget actionButton({String? title, VoidCallback? onPressed}) {
+    return Expanded(
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: BorderRadius.circular(5.0),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+              color: scaffoldBackgroundLight,
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  title!,
+                  style: const TextStyle(color: Colors.black, fontSize: 18),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
